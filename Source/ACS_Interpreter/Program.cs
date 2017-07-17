bf1359654f29bbc1b6c7c3154c4ceb6fc448fec3
asixa <asixa@foxmail.com>
2017-07-17 21:30:21 +0800

    Asixa

---
+++ Source/ACS_Interpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACS_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Source/ACS_Lexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACS_Lexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
