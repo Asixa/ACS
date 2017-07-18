a6039d1c03a5717f853645a99d21dc8cba46b14c
fangxm233 <1126716733@qq.com>
2017-07-18 21:25:01 +0800

    fangxm

---
+++ Source/ACS_Lexer/Token.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACS_Lexer
+{
+    public enum Types
+    {
+        Identifier,
+        Number,
+        String,
+        Bool,
+        Float
+    }
+
+    public abstract class Token
+    {
+        public static readonly int EOF = -1;
+        public static string EOL = "\\n";
+        public Types type;
+        int line_number;
+
+        protected Token(int line)
+        {
+            line_number = line;
+        }
+        public int GetLineNumber()
+        {
+            return line_number;
+        }
+        public int GetNumber()
+        {
+            throw new Exception("not number token");
+        }
+        public float GetFloat()
+        {
+            throw new Exception("not float token");
+        }
+        public bool GetBool()
+        {
+            throw new Exception("not bool token");
+        }
+        public string GetText()
+        {
+            return "";
+        }
+        public Types GetTokenType()
+        {
+            return type;
+        }
+    }
+}
